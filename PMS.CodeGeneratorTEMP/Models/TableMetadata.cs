8e65adcc36ac3c2e65d84f48a8e89fa984f73a39
Muhammad Ibad Sohail <ibad20sohail@gmail.com>
2026-07-15 17:07:56 +0500

    fixed merging conflict for project-setup

--- PMS.CodeGeneratorTEMP/Models/TableMetadata.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PMS.CodeGenerator.Models
-{
-    public class TableMetadata
-    {
-        public string Name { get; set; }
-        public List<ColumnMetadata> Columns { get; set; }
-        public List<ForeignKeyMetadata> ForeignKeys { get; set; } = new();
-    }
-}
