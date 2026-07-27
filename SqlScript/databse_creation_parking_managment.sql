7d512416cdc413f9cafc3f03f1548f58d45130a7
Muhammad Ibad Sohail <ibad20sohail@gmail.com>
2026-07-27 20:30:50 +0500

    added getuserbyusername sp

--- SqlScript/databse_creation_parking_managment.sql
+++ SqlScript/databse_creation_parking_managment.sql
@@ -93,7 +93,7 @@ ALTER TABLE billings ADD CONSTRAINT FK_billings_payment_methods FOREIGN KEY (pay
 --SP START--
 GO
 CREATE OR ALTER PROCEDURE usp_add_user
-@user_name VARCHAR(30), @password VARCHAR(30), @role_id INT, @email VARCHAR(30), @contact_no VARCHAR(20)
+@user_name VARCHAR(30), @password VARCHAR(30), @role_id INT, @email VARCHAR(30), @contact_no VARCHAR(20) = NULL
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -305,6 +305,7 @@ BEGIN
 	SELECT 'Parking space has been deleted' AS Message;
 	RETURN;
 END
+---------AUTH SP'S--------------
 GO
 CREATE OR ALTER PROCEDURE usp_login_user
 @user_name VARCHAR(30), @password VARCHAR(500)
@@ -322,12 +323,14 @@ BEGIN
 	DECLARE @stored_hashed_password VARBINARY(64);
 	DECLARE @stored_role_name VARCHAR(20);
 	DECLARE @stored_role_id INT;
+	DECLARE @stored_email VARCHAR(30);
 
 	SELECT 
 		@stored_user_id = u.user_id,
 		@stored_hashed_password = u.password_hash,
 		@stored_role_name = r.name,
-		@stored_role_id = u.role_id
+		@stored_role_id = u.role_id,
+		@stored_email = u.email
 	FROM users u INNER JOIN roles r ON u.role_id = r.role_id
 	WHERE u.user_name = @user_name AND u.is_active = 1;
 
@@ -341,6 +344,34 @@ BEGIN
 		@user_name AS user_name,
 		@stored_role_id AS role_id,
 		@stored_role_name AS role_name;
+END
+GO
+CREATE OR ALTER PROCEDURE usp_get_user_by_username
+@user_name VARCHAR(30)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET XACT_ABORT ON;
+
+	IF NULLIF(TRIM(@user_name), '') IS NULL
+	BEGIN
+		;THROW 50001, 'Username is required.',1;
+	END
+
+	DECLARE @stored_email VARCHAR(30);
+	
+	SELECT @stored_email = email FROM users WHERE user_name = @user_name;
+	IF @@ROWCOUNT = 0
+	BEGIN
+		;THROW 50001, 'Invalid credentials.',2;
+	END
+	IF NULLIF(TRIM(@stored_email),'') IS NULL
+	BEGIN
+		DECLARE @error_message VARCHAR(30) = FORMATMESSAGE('Email of "%s" not found', @user_name)
+		;THROW 50001, @error_message,3;
+	END
+	SELECT
+		@stored_email AS email;
 END
 --SP END--
 
@@ -400,5 +431,5 @@ GO
 EXEC usp_add_role 'admin';
 EXEC usp_add_role 'operator';
 
-EXEC usp_add_user @user_name = 'operator',@password = 'Operator@123', @role_id = 2;
+EXEC usp_add_user 'operator','Operator@123', 2,'operator@yopmail.com';
 --INSERT END--
